dd29fa3ac789dd9c027157eec1c9daf6daeaa882
ankitt-gupta <ankit.ag1782@gmail.com>
2025-06-18 19:00:46 +0530

    Added Producer: deliveryboy and consumer: end-user

---
+++ deliveryboy/src/main/java/com/ankit/deliveryboy/config/AppaConstants.java
@@ -0,0 +1,7 @@
+package com.ankit.deliveryboy.config;
+
+public class AppaConstants {
+
+	public static final String  LOCATION_TOPIC_NAME = "location-update-topic";
+	
+}
---
+++ deliveryboy/src/main/java/com/ankit/deliveryboy/service/KafkaService.java
@@ -0,0 +1,24 @@
+package com.ankit.deliveryboy.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import com.ankit.deliveryboy.config.AppaConstants;
+
+@Service
+public class KafkaService {
+
+	private Logger logger = LoggerFactory.getLogger(KafkaService.class);
+
+	@Autowired
+	private KafkaTemplate<String, String> kafkaTemplate;
+
+	public boolean updateLocation(String location) {
+		this.kafkaTemplate.send(AppaConstants.LOCATION_TOPIC_NAME, location);
+		this.logger.info("Message Produces");
+		return true;
+	}
+}
